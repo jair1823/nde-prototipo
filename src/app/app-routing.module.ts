90b96c398b8f5d7da891af7cdcc6da176cdf4db3
jair1823 <cordero1823.cr@gmail.com>
2019-08-29 17:24:39 -0600

    Navbar construido, con routerLink.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,49 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { GroupNewsComponent } from './component/group-news/group-news.component';
+import { ExecutiveStaffComponent } from './component/executive-staff/executive-staff.component';
+import { TeacherStaffComponent } from './component/teacher-staff/teacher-staff.component';
+import { GalleryComponent } from './component/gallery/gallery.component';
+import { UserLoginComponent } from './component/user-login/user-login.component';
+import { ContactInformationComponent } from './component/contact-information/contact-information.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
+    path:'home',
+    component: HomeComponent
+  },
+  {
+    path:'groups',
+    component: GroupNewsComponent
+  },
+  {
+    path:'executives',
+    component: ExecutiveStaffComponent
+  },
+  {
+    path:'teachers',
+    component: TeacherStaffComponent
+  },
+  {
+    path:'gallery',
+    component: GalleryComponent
+  },
+  {
+    path:'contact',
+    component: ContactInformationComponent
+  },
+  {
+    path:'users',
+    component: UserLoginComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
